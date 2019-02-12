247da10e1ea7dd2d2aca20e076a945c98ad9c8fc
Muruganandham K <muruganandham23@gmail.com>
2019-02-12 20:28:04 +0530

    Added left menu and page redirection

---
+++ lib/ContactUs.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ContactUs extends StatelessWidget {
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact us'),
+      ),
+      body: Center(
+        child: Text('Contact us'),
+      ),
+    );
+  }
+}
+
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -3,6 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swivy/counter_bloc.dart';
 import 'package:swivy/counter_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import './TermsConditions.dart';
+import './AboutUs.dart';
+import './ContactUs.dart';
+
 
 class HomePage extends StatefulWidget {
   final FirebaseUser fbuser;
@@ -40,14 +46,45 @@ class CounterWidget extends StatelessWidget {
 
   final HomePage widget;
 
+  BuildContext get context1 => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the home_page object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: new Drawer(
+          child: new ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          new DrawerHeader(
+              child: Center(child: Text('Drawer Header')),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              )),
+         _tile(context, 'Home', '', Icons.home, _goHome),
+          new Divider(),
+          new ListTile(
+            title: new Text('About us'),
+            onTap: () {
+              Navigator.pop(context);
+              _aboutus(context);
+            },
+          ),
+          new Divider(),
+          _tile(context, 'Contact us', '', Icons.smartphone, _contactus),
+          new Divider(),
+          new ListTile(
+            title: new Text('Terms & Condition'),
+            onTap: () {
+              Navigator.pop(context);
+              _termsConditions(context);
+            },
+          ),
+          new Divider(),
+        ],
+      )),
       body: BlocBuilder(
           bloc: BlocProvider.of<CounterBloc>(context),
           builder: (context, CounterState state) {
@@ -67,6 +104,7 @@ class CounterWidget extends StatelessWidget {
                       Navigator.pop(context);
                     },
                   ),
+                  new Icon(FontAwesomeIcons.solidLaugh),
                   SizedBox(
                     height: 40,
                   ),
@@ -108,4 +146,41 @@ class CounterWidget extends StatelessWidget {
       ),
     );
   }
+
+  //user defind methods
+
+  void _contactus() async {
+    print("Contact us...");
+    Navigator.of(context1).push(MaterialPageRoute(builder: (context) => ContactUs()));
+  }
+
+  void _goHome() async {
+    print("Go to Home...");
+  }
+
+  void _aboutus(BuildContext context1) async {
+    print('about us..');
+    Navigator.push(context1,
+    MaterialPageRoute(builder: (context) => AboutUs()),); 
+  }
+
+  void _termsConditions(BuildContext context1) async {
+    print('terms and conditions..');
+    Navigator.push(context1,
+    MaterialPageRoute(builder: (context) => TermsConditions()),);  
+  }
+
+  //custom list tile
+  ListTile _tile(BuildContext context1, String title, String subtitle, IconData icon, VoidCallback methodname) => ListTile(
+      title: Text(title,),
+      subtitle: (subtitle.length == 0)? null : Text(subtitle),
+      onTap: () {
+            Navigator.pop(context1);
+            methodname();
+            },
+      leading: Icon(
+        icon,
+        color: Colors.blue[400],
+      ),
+    );
 }
